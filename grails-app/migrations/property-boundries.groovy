f23ab849942af109302e661003eb9e9b18f57dd7
Andre Boorsma <andre.boorsma@tno.nl>
2015-04-21 16:25:15 +0200

    dbm-gorm-diff generated property-boundries (migrantions)

--- grails-app/migrations/property-boundries.groovy
+++ grails-app/migrations/property-boundries.groovy
@@ -1,6 +1,14 @@
-changeSet(id: "propertyBoundries", author: "ferry") {
-    addColumn(tableName: "property") {
-        column(name: "minimum_value", type: "numeric(19, 2)")
-        column(name: "maximum_value", type: "numeric(19, 2)")
-    }
+databaseChangeLog = {
+
+	changeSet(author: "aboorsma (generated)", id: "1429626136235-1") {
+		addColumn(tableName: "property") {
+			column(name: "maximum_value", type: "numeric(19, 2)")
+		}
+	}
+
+	changeSet(author: "aboorsma (generated)", id: "1429626136235-2") {
+		addColumn(tableName: "property") {
+			column(name: "minimum_value", type: "numeric(19, 2)")
+		}
+	}
 }
